f04422361ca709d1deda53f4caf920f618541ba6
fineundefined <a_kolesnikov@utb.cz>
2022-11-14 22:41:38 +0200

    edited main fun added new functions

--- Morseovka/main.py
+++ Morseovka/main.py
@@ -4,67 +4,64 @@ from GetFile import *
 from TeamName import *
 import telebot
 
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('5614828416:AAF4WNZqZf1_BRqmDPFQIwye5CEft5a0MPY')
 
 if __name__ == "__main__":
     os.system("title Morseovka")
     os.system("mode con cols=101 lines=30")
 
 bot.set_my_commands([
-    telebot.types.BotCommand("/decode", "'/decode + text'"),
-    telebot.types.BotCommand("/encode", "'/encode + text'")
+    telebot.types.BotCommand("/encode", "'/encode + text'"),
+    telebot.types.BotCommand("/decode", "/decode + morse code")
 ])
+
+
+@bot.message_handler(commands=['/encode', '/decode'])
+def send_info_message(message):
+    if message.text == "/encode":
+        bot.send_message(message, "write some text to encode")
+    elif message.text == "/decode":
+        bot.send_message(message, "write morse code to decode")
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    userInput = message.text
-    write_message_fun(userInput,message)
+    user_Input = message.text
+    user_Input = user_Input.split(" ", 1)
+    fisrt_command = user_Input[0]
+    del user_Input[0]
 
+    user_Input = ''.join(user_Input)
+    dot = '.'
+    dash = '-'
+    try:
+        user_Input = user_Input.split(' /', 2)
+        dot = user_Input[1]
+        dash = user_Input[2]
+        del user_Input[1:3]
+    except:
+        pass
 
-def write_message_fun(userInput,message):
-    splitted_user_input = userInput.split(" ", 1)
-    print(message.from_user.first_name, message.text)
+    print(message.from_user.first_name, "[", message.from_user.id, "]:", message.text)
 
-    if splitted_user_input[0] == '/decode':
-        del splitted_user_input[0]
-        splitted_user_input = ' '.join(splitted_user_input)
+    if user_Input[0] != "":
 
-        print("decoding from morse code\n")
-        bot.send_message(message.from_user.id, Morse.decodeMorse(splitted_user_input))
-
-    elif splitted_user_input[0] == "/encode":
-
-        del splitted_user_input[0]
-        splitted_user_input = ' '.join(splitted_user_input)
-
-        print("encoding from morse code\n")
-        bot.send_message(message.from_user.id, Morse.encodeMorse(splitted_user_input))
-
-    elif splitted_user_input[0] == "/file":
-        del splitted_user_input[0]
-        splitted_user_input = ' '.join(splitted_user_input)
-        fileTEXT = GetFile.GetTextFromTXT(splitted_user_input)
-
-        if fileTEXT != FileNotFoundError:
-            inputChoose = (input("Decode text in file or encode [d/e]"))
-
-            if inputChoose == "d":
+        match fisrt_command:
+            case "/decode":
+                user_Input = ' '.join(user_Input)
 
                 print("decoding from morse code\n")
-                print(Morse.decodeMorse(fileTEXT), "\n")
+                bot.send_message(message.from_user.id, Morse.decodeMorse(user_Input, dot, dash))
+            case "/encode":
+                user_Input = ' '.join(user_Input)
 
-            elif inputChoose == "e":
+                print("encoding from morse code\n")
+                bot.send_message(message.from_user.id, Morse.encodeMorse(user_Input, dot, dash))
 
-                print("encoding to morse code\n")
-
-                print(Morse.encodeMorse(fileTEXT), "\n")
-
-            else:
-                print("!" * 99)
-                print(
-                    "File adress is incorrect!\nTry other forms of address(C:\\\..\\\..\\\ | C:\..\..\ | C:/../../) or check if the address is correct"
-                )
-                print("!" * 99, "\n")
+            case _:
+                bot.send_message(message.from_user.id, "Command is incorrect")
+    else:
+        bot.send_message(message.from_user.id, "You cannot encode or decode nothing")
 
 
 bot.polling(none_stop=True, interval=0)
