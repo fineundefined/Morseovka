dde536d4823a3f34d80984dc51b3f582c4e003ce
fineundefined <a_kolesnikov@utb.cz>
2022-11-20 20:02:16 +0200

    first workflow

---
+++ .gith/Morse.py
@@ -0,0 +1,41 @@
+class Morse:
+
+    def decodeMorse(morseCode, morse_code):
+
+        morseCodeDict = morseCode.split(" ")
+
+        output = []
+
+        for morse in morseCodeDict:
+            try:
+                output.append(morse_code[morse])
+            except KeyError:
+                if morse == "":
+                    output.append(" ")
+                else:
+                    pass
+
+        output = "".join(output)
+        return output.lower()
+
+
+    def encodeMorse(toEncode, morse_code):
+
+        morse_code_inverted = {v: k for k, v in morse_code.items()}
+
+        toEncodeDict = list(toEncode)
+
+        output = []
+
+        for char in toEncodeDict:
+            try:
+                output.append(morse_code_inverted[char.upper()])
+            except KeyError:
+                if char == " ":
+                    output.append(" ")
+                else:
+                    pass
+
+        return " ".join(output)
+
+    pass
